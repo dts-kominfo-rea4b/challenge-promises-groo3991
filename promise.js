b682ed84888c18771f4de053f0cccbfa5981b221
Januar_Dwi_Admaja <dwik@example.com>
2022-09-18 21:29:37 +0700

    membuat promise dengan nama promiseOutput

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+async function promiseOutput(emosi) {
+  const theater1 = await promiseTheaterIXX();
+  const theater2 = await promiseTheaterVGC();
+  const gabung = theater1.concat(theater2);
+  return new Promise((resolve) => {
+    if (emosi === "marah") {
+      const marah = gabung.filter((item) => item.hasil === "marah").length;
+      resolve(marah);
+    } else if (emosi === "tidak marah") {
+      const tidakMarah = gabung.filter((item) => item.hasil === "tidak marah").length;
+      resolve(tidakMarah);
+    }
+  });
+};
 module.exports = {
   promiseOutput,
 };
